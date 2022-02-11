6f1d07cfea571ddcdb785ecc3097af22cc3e0cbc
SHI3DO <61821471+SHI3DO@users.noreply.github.com>
2022-02-11 23:51:57 +0900

    added

--- sikfa.py
+++ sikfa.py
@@ -1,6 +1,34 @@
 import torch
-import numpy as np
+import math
+
+dtype = torch.float
+device = torch.device('cpu')
 
 
-def find(x: list, y: list):
-    print(x, y)
+def find(x: list, y: list, weightsnum: int, trainnum: int, learning_rate: float):
+    x = torch.FloatTensor(x)
+    y = torch.FloatTensor(y)
+
+    coefficient_list = []
+    for i in range(0, weightsnum):
+        coefficient_list.append(torch.randn((), device=device, dtype=dtype, requires_grad=True))
+
+    for k in range(trainnum):
+        y_pred = 0
+        for h in range(0, len(coefficient_list)):
+            y_pred += coefficient_list[h]*x**h
+
+        loss = (y_pred - y).pow(2).sum()
+        if k % 100 == 99:
+            print(k, loss.item())
+
+        loss.backward()
+
+        with torch.no_grad():
+            for h in range(0, len(coefficient_list)):
+                coefficient_list[h] -= learning_rate * coefficient_list[h].grad
+
+                coefficient_list[h].grad = None
+
+    for h in range(0, len(coefficient_list)):
+        print(coefficient_list[h].item())
